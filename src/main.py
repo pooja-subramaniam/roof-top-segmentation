04b0b58ce08aae828c10875585038f42f6c91f37
Pooja Subramaniam <poojas2086@gmail.com>
2023-02-03 15:57:08 +0100

    get dataloaders to be used for training

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from yaml import safe_load
 import os
 from pathlib import Path
 
-from data import DidaSegmentationDataset
+from data import DidaSegmentationDataset, get_dataloader
 from utils import save_dict
 
 CONFIG_FILENAME = 'config.yaml'
@@ -14,6 +14,12 @@ def main():
     with open(CONFIG_FILENAME) as config_file:
         config = safe_load(config_file) 
     save_dict(config, LOG_FOLDER / 'config.json')
+
+    data_directory = TOP_DIRECTORY / config['data dir']
+
+    # Create the dataloader
+    dataloaders = get_dataloader(data_directory, config['images dir'], config['labels dir'],
+                                 seed=config['seed'], batch_size=config['batch size'])
 
 
 if __name__ == '__main__':
