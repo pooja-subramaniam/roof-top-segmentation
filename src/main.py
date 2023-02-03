2fc94bb4e7a9ceeaf73e3f12b007ec75ca00ff59
Pooja Subramaniam <poojas2086@gmail.com>
2023-02-03 15:55:31 +0100

    logging config file

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
 from yaml import safe_load
 import os
+from pathlib import Path
 
 from data import DidaSegmentationDataset
+from utils import save_dict
 
 CONFIG_FILENAME = 'config.yaml'
-TOP_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TOP_DIRECTORY = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+LOG_FOLDER = TOP_DIRECTORY / 'models'
 
 def main():
 
     with open(CONFIG_FILENAME) as config_file:
-        config = safe_load(config_file)
+        config = safe_load(config_file) 
+    save_dict(config, LOG_FOLDER / 'config.json')
 
 
 if __name__ == '__main__':
