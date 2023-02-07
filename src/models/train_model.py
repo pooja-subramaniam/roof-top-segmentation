23d8c2291f06a0824e54dbaf2bbf0d07c2ce7891
Pooja Subramaniam <poojas2086@gmail.com>
2023-02-07 22:35:11 +0100

    training model module added with helper functions in utils

---
+++ src/models/train_model.py
@@ -0,0 +1,89 @@
+import time
+import copy
+from typing import Any
+from pathlib import Path
+
+import torch
+import utils
+
+
+def train(model: Any, criterion: Any, dataloaders: Any,
+          optimizer: Any, log_folder: Path, num_epochs: int) -> Any:
+    """Training module with pre-trained weights
+    Args:
+        model: pretrained model that needs fine-tuning
+        criterion: loss function to be used
+        dataloaders: train and val dataloader objects for training
+        optimizer: torch optimizer object
+        log_folder: folder to log training performance
+        num_epochs: total number of epochs to train
+    Returns:
+        trained model with best weights based on val performance
+
+    """
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss = 1e10
+
+    # Use gpu if available
+    device = utils.get_device()
+    model.to(device)
+
+    # Initialize fieldnames and epoch summary
+    fieldnames, epochsummary = utils.prep_training_log(log_folder)
+
+    start = time.time()
+    print(f"Starting training with {criterion.__class__.__name__} and {optimizer.__class__.__name__} "
+            f"optimizer with learning rate {optimizer.param_groups[0]['lr']}")
+    for epoch in range(1, num_epochs + 1):
+
+        print(f'Epoch {epoch}/{num_epochs}')
+        print('-' * 10)
+
+        batch_loss = {'train': [], 'val': []}
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            # Iterate over data.
+            for sample in iter(dataloaders[phase]):
+
+                inputs = sample['image'].to(device)
+                labels = sample['label'].to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    loss = criterion(outputs['out'], labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                batch_loss[phase].append(loss.item())
+
+        epochsummary = utils.update_epochsummary(epochsummary, epoch, batch_loss)
+        print(epochsummary)
+        utils.save_train_log(log_folder, fieldnames, epochsummary)
+
+        # update best_loss and best_model_wts
+        if epochsummary['val_loss'] < best_loss:
+            best_loss = epochsummary['val_loss']
+            best_model_wts = copy.deepcopy(model.state_dict())
+            utils.save_model(best_model_wts, log_folder)
+
+    time_elapsed = time.time() - start
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Lowest Loss: {best_loss:4f}')
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,106 @@
-from typing import Dict, Any
 import json
+import numpy as np
+from typing import Dict, Any, Tuple, List, Optional
+from pathlib import Path
+import csv
+import random
+import os
 
-def save_dict(d: Dict[str, Any], filename: str) -> None:
+import torch
+
+
+def save_dict(d: Dict[str, Any], filename: Path) -> None:
+    """Save dictionary as json file
+    Args:
+        d: data to be dumped into a json file
+        filename: filename path
+    """
     with open(filename, 'w') as f:
         json.dump(d, f)
+
+
+def verify_exists_else_create(folder: Path) -> None:
+    """Verify if the folder exists in the given path
+    otherwise, create it
+    Args:
+        folder: path of the folder
+    """
+    folder.mkdir(parents=True, exist_ok=True)
+
+def set_seed(seed: int) -> None:
+    """set all the random seeds for reproducibility
+    Args:
+        seed: value from the config file
+    """
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    random.seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
+def get_device() -> None:
+    """Get device based on GPU availability
+    """
+    return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def prep_training_log(log_folder: Path) -> Tuple:
+    """Prepare log variable and log file for training and validation loss
+    to be updated during training
+    Args:
+        log_folder: folder where log file has to be saved
+    Returns:
+        Tuple of fieldnames to be logged and empty dictionary with fieldnames
+    """
+
+    # Initialize the log file for training and val loss
+    fieldnames = ['epoch', 'train_loss', 'val_loss']
+    epochsummary = {a: [0] for a in fieldnames}
+    save_train_log(log_folder, fieldnames, epochsummary, mode='w')
+
+    return fieldnames, epochsummary
+
+
+def update_epochsummary(epochsummary: Dict[str, Any], epoch: int,
+                        batch_loss: Dict[str, float]) -> Dict[str, Any]:
+    """Update summary of epochs from batch data
+    Args:
+        epochsummary: dictionary to be updated with mean of batch loss
+        epoch: epoch number in the train loop
+        batch_loss: dictionary of the losses in the batches in the current epoch
+
+    Returns:
+        Dictionary containing updated epochsummary
+    """
+    epochsummary['epoch'] = epoch
+    phases = ['train', 'val']
+    for phase in phases:
+        epochsummary[f'{phase}_loss'] = np.mean(batch_loss[phase])
+
+    return epochsummary
+
+
+def save_train_log(log_folder: Path, fieldnames: List,
+                   epochsummary: Optional[Dict[str, Any]], mode: str='a') -> None:
+    """Save train log to log.csv in log_folder with fieldnames as header
+    and epochsummary as data
+    Args:
+        log_folder: folder where log file has to be saved
+        fieldnames: fieldnames to be used as header
+        epochsummary: summary of epoch losses to be written into log.csv
+    """
+    with open(log_folder / 'log.csv', mode, newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if mode == 'w':
+            writer.writeheader()
+        elif mode == 'a':
+            writer.writerow(epochsummary)
+
+
+def save_model(model_weights: Any, log_folder: Path):
+    """Save weights of best model
+    Args:
+        model_weights: state_dict of model
+    """
+    torch.save(model_weights, log_folder / 'weights.pt')
