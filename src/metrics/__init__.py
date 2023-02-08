4f1689633ac3c8d8fdd762b22ed1f15eb67b3b8c
Pooja Subramaniam <poojas2086@gmail.com>
2023-02-08 21:01:42 +0100

    adding dice metric

---
+++ src/metrics/__init__.py
@@ -0,0 +1,1 @@
+from metrics.dice import dice_coefficient
---
+++ src/metrics/dice.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def dice_coefficient(y_true, y_pred):
+    """Calculate dice coefficient 
+    Args:
+        y_true: true labels
+        y_pred: predictions
+    Returns:
+        Dice coefficient
+    """
+    smooth = 1.    
+    y_true_f = y_true.flatten()
+    y_pred_f = y_pred.flatten()
+    intersection = np.sum(y_true_f * y_pred_f)
+
+    return (2. * intersection + smooth) / (np.sum(y_true_f) +
+                    np.sum(y_pred_f) + smooth)
